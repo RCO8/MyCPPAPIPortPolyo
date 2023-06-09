1a4db3d8cfe8f0cccfa548c605e881f0cb5fd865
RCO8 <39510436+RCO8@users.noreply.github.com>
2023-06-09 15:06:34 +0900

    5블럭 테트리스

---
+++ 5블럭 테트리스/5블럭 테트리스/resource.h
@@ -0,0 +1,19 @@
+//{{NO_DEPENDENCIES}}
+// Microsoft Visual C++에서 생성한 포함 파일입니다.
+// 5블럭 테트리스.rc에서 사용되고 있습니다.
+//
+#define IDB_BRICK                       101
+#define IDR_MENU1                       102
+#define IDB_BLANK                       103
+#define ID_START                        40002
+
+// Next default values for new objects
+// 
+#ifdef APSTUDIO_INVOKED
+#ifndef APSTUDIO_READONLY_SYMBOLS
+#define _APS_NEXT_RESOURCE_VALUE        104
+#define _APS_NEXT_COMMAND_VALUE         40003
+#define _APS_NEXT_CONTROL_VALUE         1001
+#define _APS_NEXT_SYMED_VALUE           101
+#endif
+#endif
